b38645906f224a3d2646ef4c1cdfb14e6322f67b
Alessander Franca <alessander70@gmail.com>
2019-03-20 20:25:55 -0300

    [Hooks] counter using hooks state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-counter-container">
+          <button
+            className="App-counter-button"
+            onClick={() => {
+              setCounter(counter - 1);
+            }}
           >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+            -
+          </button>
+          <div className="App-counter-text">{counter}</div>
+          <button
+            className="App-counter-button"
+            onClick={() => {
+              setCounter(counter - 1);
+            }}
+          >
+            +
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default App;
